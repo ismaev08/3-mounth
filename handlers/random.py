d7a32c28f855c5775621bd19c823d437b5b2c6a0
selimismaev <ismaevrashid83@gmail.com>
2024-11-13 18:51:33 +0600

    hww

--- handlers/random.py
+++ handlers/random.py
@@ -2,9 +2,11 @@ from aiogram import Router, types
 from aiogram.filters import Command
 import random
 
+
 random_router = Router()
 
 names = ["Алексей", "Мария", "Иван", "Елена", "Сергей"]
+
 
 @random_router.message(Command("random"))  # Декоратор должен быть перед функцией
 async def random_command(message: types.Message):
